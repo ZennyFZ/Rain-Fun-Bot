6fa4c29ec7ef078f00b55d2facc6b877d1b6d51e
Zen Nguyễn <nhandinhdo291@gmail.com>
2020-08-30 13:07:25 +0700

    Add files via upload

--- Setting/config.js
+++ Setting/config.js
@@ -1,15 +1,13 @@
-module.exports = {
-  AccountName: 'your_username',
-  Password: 'your_password',
-  SharedSecret: 'your_sharedsecret',
-  AdminID: '76561198349964534',
+﻿module.exports = {
+  AccountName: 'Your_Username_Bot',
+  Password: 'Your_Password_Bot',
+  SharedSecret: 'Your_SharedSecret_Bot',
+  AdminID: 'Your_SteamID64',
   rememberPassword: 'True',
   AutoAcceptFriend: 'True',
   machineName: 'Acer Predator Triton',
   x6TimeLimit: 120000,
   x6CommentDelay: 5000,
-  RestartPath: '"D:/Documents/Rain"',
-  StartBatch: "Start.bat",
   Status: '1',   //0 = Offline
                //1 = Online
                //2 = Busy
@@ -20,9 +18,10 @@ module.exports = {
                //7 = Invisible
                //8 = Max
   ui: 1,      //1= BigPicture 2=Mobile 3=Web
+  WeatherAPI: 'Your_Weather_API',
   CommandError: ['oof you use incorrect command (╯°□°)╯︵ ┻━┻','incorrect command pls try again O.o','aww snap there is no command like that, try again :D','incorrect command, !help to use it correctly >.O','aww... you do a incorrect command, try again ^^','if you see this message, you just use incorrect command :P','aha incorrect command, !help may be helpful ^^','i dont understand your command, please do it correctly (╯°□°)╯︵ ┻━┻'],
   Welcome: '/me thanks for adding me, !help to see all commands >.O',
   Owner: '/me if you have any questions, feel free to contract my owner >.O \n https://steamcommunity.com/profiles/76561198349964534',
-  CommandAdmin: '/pre Commands: \n !Neko - random neko image (=^∇^*=) \n !comment - Bot will comment your custom comment \n !cmt6x6 - Bot will comment your custom comment 6 times (2 minute per !cmt6x6) \n (Why there is 2 minute limit ? because Rain avoid to spam steam api) \n !chat - you can use this command to chat with my master though me to report problem or anything >.O \n !hentai - who gonna use it ? \n \n Example: !comment Hello or !cmt6x6 Hello or !chat hello \n \n Note: Bot wont comment if your comment is too long (steam limit) or bot is reached steam comment limit \n \n Other Command: \n !redeemkey - send a game key which you dont use to bot (・∀・) \n !addlicense - help bot by adding free subid to bot ~(=^–^) \n !redeemWallet - similar to !redeemKey but for wallet (⁎˃ᆺ˂) \n \n Admin Command: \n !block/!unblock/!remove + steamID - Block/unlock/remove a user \n !restart - restart bot \n !acomment - comment on that profile ID \n !send - send message to user \n \n Example: !send ID Message',
-  Command: '/pre Commands: \n !Neko - random neko image (=^∇^*=) \n !comment - Bot will comment your custom comment \n !cmt6x6 - Bot will comment your custom comment 6 times (2 minute per !cmt6x6) \n (Why there is 2 minute limit ? because Rain avoid to spam steam api) \n !chat - you can use this command to chat with my master though me to report problem or anything >.O \n !hentai - you must not use it o(･´д･｀o)))三 \n \n Example: !comment Hello or !cmt6x6 Hello or !chat hello \n \n Note: Bot wont comment if your comment is too long (steam limit) or bot is reached steam comment limit \n \n Other Command: \n !redeemkey - send a game key which you dont use to bot (・∀・) \n !addlicense - help bot by adding free subid to bot ~(=^–^) \n !redeemWallet - similar to !redeemKey but for wallet (⁎˃ᆺ˂)'
+  CommandAdmin: 'Commands|Usage: \n !Neko - random neko image (=^∇^*=) \n \n !Weather - check City Weather | !Weather Halifax \n \n !animal - random cat,dog,bird image | !animal cat \n \n !emailcheck - check data breach of your email | !emailcheck abcxyz@gmail.com \n \n !rps - play Rock Paper Scissor with Rain | !rps Rock/Paper/Scissor/Score \n \n !comment - Bot will comment your custom comment | !comment hello \n \n !cmt6x6 - Bot will comment your custom comment 6 times (2 minute per !cmt6x6) | !cmt6x6 hello \n \n !acomment - comment your custom message on another profile | !acomment hello \n \n !chat - you can use this command to chat with my master though me to report problem or anything >.O | !chat Hello \n \n !hentai - idk. you can try o(･´д･｀o)))三 \n \n Note: Bot wont comment if your comment is too long (steam limit) or bot reached steam comment limit \n \n Other Command: \n !redeemkey/!addlicense/!redeemWallet - redeem game,addlicense,wallet \n \n Admin Command: \n !block/!unblock/!remove + steamID - Block/unlock/remove a user \n !restart - restart bot \n !send - send message to user | !send [ID] [Message]',
+  Command: 'Commands|Usage Commands: \n !Neko - random neko image (=^∇^*=) \n \n !Weather - check City Weather | !Weather Halifax \n \n !animal - random cat,dog,bird image | !animal cat \n \n !emailcheck - check data breach of your email | !emailcheck abcxyz@gmail.com \n \n !rps - play Rock Paper Scissor with Rain | !rps Rock/Paper/Scissor/Score \n \n !comment - Bot will comment your custom comment | !comment Hello \n \n !cmt6x6 - Bot will comment your custom comment 6 times (2 minute per !cmt6x6) | !cmt6x6 hello \n \n !acomment - comment your custom message on another profile | !acomment hello \n \n !chat - you can use this command to chat with my master though me to report problem or anything >.O | !chat Hello \n \n !hentai - idk. you can try o(･´д･｀o)))三 \n \n Note: Bot wont comment if your comment is too long (steam limit) or bot reached steam comment limit'
 };
